3ed09c2237f6e602205b897832457095701a4c0a
gegor463 <gegor463@gmail.com>
2025-04-06 14:56:55 +0300

    добавили новый скрипт,который отвечает за движение трафика

---
+++ Assets/Scripts/TrafficMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficMove : MonoBehaviour
+{
+    private List<GameObject> _wayPoints;
+    private int _currentWayPoint = 0;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
