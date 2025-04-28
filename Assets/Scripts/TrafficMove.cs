7a594f67c94e81ec6852a46060ae0da6681c3263
gegor463 <gegor463@gmail.com>
2025-04-28 18:35:44 +0300

    Дописали скрипт движения трафика

--- Assets/Scripts/TrafficMove.cs
+++ Assets/Scripts/TrafficMove.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TrafficMove : MonoBehaviour
 {
-    private List<GameObject> _wayPoints;
+    [SerializeField] private List<GameObject> _wayPoints;
     private int _currentWayPoint = 0;
+    [SerializeField] private float _speed = 10.0f;
+    private Rigidbody _rigidbody;
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        Debug.Log(_wayPoints.Count);    
     }
 
     void Update()
     {
-        
+        if(Vector3.Distance(this.transform.position, _wayPoints[_currentWayPoint].transform.position) < 3.0f)
+        {
+            if(_currentWayPoint < _wayPoints.Count - 1)
+            {
+                _currentWayPoint++;
+            }
+            else
+            {
+                _currentWayPoint = 0;
+            }
+            Debug.Log(_currentWayPoint);
+            
+        }
+   
+
+        transform.LookAt(_wayPoints[_currentWayPoint].transform);
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
     }
 }
